e2e4fcd596d5c39dde889e42f9ad9945c9b3aa51
AngelBiondi <48496612+AngelBiondi@users.noreply.github.com>
2019-05-09 12:10:49 -0400

    up

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://angel:angel@cluster0-s3ara.mongodb.net/test?retryWrites=true'
+    // 'mongodb://localhost/soccer' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
